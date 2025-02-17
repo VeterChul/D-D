5efbd9d99f8784be3077066fc3a0d39f6ef9a2a1
veter <veterculinin@gmail.com>
2025-02-17 23:13:38 +0300

    Черновое готовое

--- n_kyb.py
+++ n_kyb.py
@@ -1,8 +1,9 @@
-﻿import random as r
-#from inscriptions import start_mess, error_mess, h_a, h
+﻿import re
+import random as r
 
 start_mess = "Преветствуем путешествиники или мастера  в симуляторе кубов D&D.\nЧтобы прочесть инструкцию введите 'h'"
 error_mess = "Произошла техническая ошибка.\nВведите куб повторно или 'h' для порлучения справки"
+
 
 list_mode = ["s", "b", "r"]
 
@@ -24,7 +25,6 @@ list_def = {
     'throw' : (lambda mas : throw(mas)),
     }
 
-s = input()
 old_s = (0, 0)
 
 
@@ -61,7 +61,7 @@ def svipe_mode(mas):#сменить мод
     except:
         print(error_mess, "Ошибка изменения мода. Проверьте првильность ввода.\nНапишите ref для получения справки.")
 
-def print_saved(mas):
+def print_saved(mas):#Вывод сохраненных кубов
     global list_cub
     if len(mas) == 1:
         mas = [0, len(list_cub) - 1]
@@ -70,7 +70,18 @@ def print_saved(mas):
         print(f"    {i}:{list_cub[i][0]}k{list_cub[i][1]}")
 
 def new_cub(mas):#создать новый куб
-    pass
+    global list_cub
+    
+    if re.search("[0-9]*[dkк][0-9]+", mas[0]):
+        list_cub.append(open_ky(mas[0]))
+        print(f"Записан новый куб под номером {len(list_cub)}. Значение {list_cub[-1][1]}k{list_cub[-1][1]}")
+        return 0
+    if int(mas[0]) - 1 < len(list_cub):
+        list_cub[int(mas[0]) - 1] = open_ky(mas[1])
+        print(f"Куб номер {mas[0]} перезаписан. Новое значение {list_cub[int(mas[0]) - 1][1]}k{list_cub[int(mas[0]) - 1][1]}")
+    else:
+        list_cub.append(open_ky(mas[1]))
+        print(f"Записан новый куб под номером {len(list_cub)}. Значение {list_cub[-1][1]}k{list_cub[-1][1]}")
 
 def __again(mas):#ентер
     global mode, old_s
@@ -88,13 +99,13 @@ def throw_again(mas):#перекинуть последний куб
     global old_s
     kyb(old_s[1], old_s[0], mas[-1])
 
-def print_help():
+def print_help():#Вывод help
     global list_mode, mode
     h = f"Вы находитесь в режиме {list_mode[mode]}.\nДля получения справки пропишите 'ref'"
     print(h)
     return 0
 
-def print_ref():
+def print_ref():#Вывод справки
     global list_mode, mode
     h = "Заглушка"
     print(h)
@@ -106,20 +117,27 @@ def open_ky(ky):
 
 print(start_mess)
 
+s = input()
+
 while s != 'q':
     try:
-        chit = s[0] == " "
-        s = s.split()
-        if mode in (0, 2):
-            if s[0] in list_com:
-                list_def[s[0]](s[1:].append(chit))
-            else:
-                if len(s) == 1:
+        if s != "":
+            chit = s[0] == " "
+            s = s.split()
+            s.append(chit)
+        else:
+            chit = False
+            s = ["", chit]
+        if s[0] in list_com:
+                list_def[s[0]](s[1:])
+        else:    
+            if mode in (0, 2):
+                if len(s) == 2:
                     kyb(int(s[0]), chit = chit)
                 else:
                     kyb(int(s[1]), int(s[0]), chit = chit)
-        elif mode in (1, ):
-            throw([s[0], chit])
+            elif mode in (1, ):
+                throw([s[0], chit])
     except:
         print(error_mess,"Ошибка не известна, код 1") 
     finally:
